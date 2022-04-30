6a1d2e17a1967c7a74a1461b4e74591ee151387d
229024527 <229024527@qq.com>
2022-04-30 15:10:10 +0000

    bugfix

--- 8/8.cpp
+++ 8/8.cpp
@@ -6,8 +6,12 @@
 //["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 //
 #include <iostream>
+#include <vector>
+#include <cassert>
 
 using namespace std;
+
+class Solution{
 private:
 	const string letterMap[10] = {
 		" ", 	//0
@@ -23,21 +27,29 @@ private:
 	};
 	vector<string> res;
 	//s中保存了此时从digits[0...index-1]翻译得到的一个字母字符串
-	//寻找和digists[index]匹配的字母，获得digits[0...index]翻译得到的解
+	//寻找和digits[index]匹配的字母，获得digits[0...index]翻译得到的解
 	void findCombination(const string &digits, int index, const string &s){
+
+		cout<<index<<" : "<<s<<endl;
+
 		if(index == digits.size()){
 			//保存s
 			res.push_back(s);
+			cout<<"get "<<s<<" , return"<<endl;
 			return;
 		}
 
 		char c = digits[index];
 		assert(c >= '0' && c <= '9' && c != '1');
 		string letters = letterMap[c - '0'];
-		for(int i=0; i<letters.size(); i++)
+		for(int i=0; i<letters.size(); i++){
+			cout<<"digits["<<index<<"] = "<<c<<" , use "<<letters[i]<<endl;
 			findCombination(digits, index+1, s+letters[i] );
+		}
+		cout<<"digits["<<index<<"] = "<<c<<" complete, return"<<endl;
+		return;
 	}
-class Solution{
+
 public:
 	vector<string> letterCombinations(string digits){
 		res.clear();
@@ -50,6 +62,10 @@ public:
 };
 
 int main(){
+	vector<string> res = Solution().letterCombinations("234");
+	for(int i=0; i<res.size(); i++)
+		cout<<res[i]<<endl;
+
 	return 0;
 }
 //递归
